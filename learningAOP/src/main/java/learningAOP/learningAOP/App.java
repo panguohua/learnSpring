841cd31646b561e3a5f29e7bec5faa94c6e7ec16
panguohua <xpok327@gmail.com>
2019-12-29 20:39:48 +0900

    init

---
+++ learningAOP/src/main/java/learningAOP/learningAOP/App.java
@@ -0,0 +1,13 @@
+package learningAOP.learningAOP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
